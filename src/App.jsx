6d12c3918879a2ec44b8f064cac7e143bd93f374
amichy-dahan <amichyd1@gmail.com>
2025-08-14 11:50:54 +0300

    make css and folder compo and make set tweet

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,46 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import xImage from './x.jpg';
 
 
-let userNmae ="amichy"
+let userName = "amichy"
 function App() {
-  const [tweet , setTweet] =useState("")
-  const [tweets , setTweets] = useState([]);
+  const [tweet, setTweet] = useState("")
+  const [tweets, setTweets] = useState([]);
+
+
+  function handeleTwet() {
+    setTweets([
+      ...tweets,
+      {
+        name: userName,
+        text: tweet,
+        date: new Date().toLocaleDateString()
+      }
+    ]);
+    setTweet("");
+  }
 
   return (
     <>
-           <div className='container-tweeter'>
-                    <input className='input-post' type="text" />
-                    <button className='btnSharePost'>Tweet</button>
-           </div>
+      <div className='container-x'>
+        <img className='xImge' src={xImage} />
+      </div>
+      {console.log(tweets)}
+      <div className='container-tweeter'>
+        <textarea
+          className='input-post'
+          rows="10"
+          value={tweet}
+          onChange={(e) => setTweet(e.target.value)}
+          placeholder="What you Have in mind .."
+        />
+        <img onClick={handeleTwet} className='btnSharePost' src={xImage} />
+      </div>
+      
+
+      <div>fsdfs</div>
     </>
   )
 }
